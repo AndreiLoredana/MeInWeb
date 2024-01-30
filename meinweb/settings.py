29721fbe36f10c078b02a2cdff7bab1e4b12d36f
Lore <theandrei.loredana@gmail.com>
2024-01-30 20:10:58 +0200

    update css

--- meinweb/settings.py
+++ meinweb/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.static',
+    'django.contrib.staticfiles',
     'base'
 ]
 
@@ -116,9 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = (BASE_DIR / 'staticfiles',
+                    )
 
 
 # Default primary key field type
